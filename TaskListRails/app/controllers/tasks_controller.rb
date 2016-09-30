b0cf5806a4b3bd464d6a44c6b3e4a65a42f0e699
Yasmin <yasminor@gmail.com>
2016-09-30 13:53:15 -0700

    added the ability to edit a task using update method

--- TaskListRails/app/controllers/tasks_controller.rb
+++ TaskListRails/app/controllers/tasks_controller.rb
@@ -11,6 +11,10 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
   def create
     @task = Task.new(task_params)
 
@@ -23,15 +27,15 @@ class TasksController < ApplicationController
     end
   end
 
-  # def update
-  #   @task = Task.find(params[:id])
-  #   if @task.update(task_params)
-  #     # SUCCESS
-  #     redirect_to tasks_path
-  #   else
-  #     render :edit
-  #   end
-  # end
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      # SUCCESS
+      redirect_to tasks_path
+    else
+      render :edit
+    end
+  end
 
   def destroy
     Task.find(params[:id]).destroy
