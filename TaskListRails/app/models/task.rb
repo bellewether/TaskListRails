6e3bb349e3a1e9289314d318098fba6a35c36b61
Yasmin <yasminor@gmail.com>
2016-10-23 10:46:41 -0700

    Generated migration to create user_id column in tasks (which adjusted the schema) and added validations to the task model

--- TaskListRails/app/models/task.rb
+++ TaskListRails/app/models/task.rb
@@ -1,3 +1,8 @@
 class Task < ActiveRecord::Base
   belongs_to :user
+
+  validates :name, presence: true
+  validates :user_id, presence: true
+  validates :completion_status, presence: true
+
 end
--- TaskListRails/db/schema.rb
+++ TaskListRails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017221325) do
+ActiveRecord::Schema.define(version: 20161023023805) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20161017221325) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.boolean  "completion_status"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
