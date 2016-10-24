3cfaa4395a522a232650f9b0a769b41a64fefb4c
Yasmin <yasminor@gmail.com>
2016-10-23 17:40:02 -0700

    Added tests for task and user models

--- TaskListRails/test/models/task_test.rb
+++ TaskListRails/test/models/task_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  test "can create a valid task" do
+    assert tasks(:post_office).valid?
+  end
+
+  test "cannot create a task with missing information" do
+    assert_not tasks(:missing_info).valid?
+    assert_includes tasks(:missing_info).errors, :name
+    assert_includes tasks(:missing_info).errors, :user_id
+    assert_includes tasks(:missing_info).errors, :completion_status
+  end
+
+  test "cannot create a task without a name" do
+    assert_not tasks(:no_name).valid?
+    assert_includes tasks(:no_name).errors, :name
+  end
+
+  test "cannot create a task with no user_id" do
+    assert_not tasks(:no_user_id).valid?
+    assert_includes tasks(:no_user_id).errors, :user_id
+  end
+
+  test "cannot create a task doesn't have a completion status" do
+    assert_not tasks(:no_completion_status).valid?
+    assert_includes tasks(:no_completion_status).errors, :completion_status
+  end
+
+  #How to test this relationship?
+  # test "a task's user_id must match the id of the user creating the task" do
+  #
   # end
 end
--- TaskListRails/test/models/user_test.rb
+++ TaskListRails/test/models/user_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  test "can create a valid user" do
+    assert users(:one).valid?
+  end
+
+  test "cannot create a user with missing information" do
+    assert_not users(:missing_info).valid?
+
+    assert_includes users(:missing_info).errors, :email
+    # assert_includes users(:missing_info).errors, :uid
+    # assert_includes users(:missing_info).errors, :provider
+  end
+
+  # test "cannot create a user without a uid" do
+  #   assert_not users(:no_uid).valid?
+  #   assert_includes users(:no_uid).errors, :uid
   # end
+
+  test "cannot create a user without a email" do
+    assert_not users(:email).valid?
+    assert_includes users(:email).errors, :email
+  end
+
+  # test "cannot create a user without a provider" do
+  #   assert_not users(:).valid?
+  #   assert_includes user.errors, :provider
+  # end
+
+  test "can create a user from github" do
+    auth_hash = OmniAuth.config.mock_auth[:github]
+    user_gh_built = User.build_from_github(auth_hash)
+
+    assert user_gh_built.valid?
+  end
+
 end
